072eb1d9bfa67cdbffea5c5f2d73ef8e1cbeea31
snsknarayana@gmail.com <snsknarayana@gmail.com>
2024-08-20 14:23:35 -0400

    complete r backend web api for CRUD functions

--- r-backend/backend.R
+++ r-backend/backend.R
@@ -7,12 +7,25 @@ if (!requireNamespace("mongolite", quietly = TRUE)) {
   install.packages("mongolite")
 }
 
+if (!requireNamespace("dotenv", quietly = TRUE)) {
+  install.packages("dotenv")
+}
+
 # Load required packages
 library(plumber)
 library(mongolite)
+library(dotenv)
+
+# Load environment variables from the .env file
+dotenv::load_dot_env()
+
+# Access the environment variables
+mongo_url <- Sys.getenv("MONGO_URL")
+mongo_db <- Sys.getenv("MONGO_DB")
+mongo_collection <- Sys.getenv("MONGO_COLLECTION")
 
 # Connect to MongoDB
-mongo <- mongo(collection = "milkingdatas", db = "dairy", url = "mongodb://localhost:27017")
+mongo <- mongo(collection = mongo_collection, db =mongo_db, url = mongo_url)
 
 tryCatch({
   count <- mongo$count()
@@ -46,22 +59,22 @@ api$handle("GET", "/", function() {
 
 # Get all milking data
 #* @get /milking
-api$handle("GET", "/api/milking", function() {
-  milking_data <- mongo$find()
+api$handle("GET", "/api/milking/", function() {
+  milking_data <- mongo$find('{}', fields =  '{"_id": 1, "Milking Number": 1, "Duration (mm:ss)": 1, "Yield (kg)": 1, "OCC (*1000 cells/ml)": 1, "Milking Interval (hh:mm)": 1, "LF": 1, "RF": 1, "LR": 1, "RR": 1, "Udder": 1, "Milk Destination": 1}')
   return(milking_data)
 })
 
 # Create new milking data
 #* @post /milking
-api$handle("POST", "/api/milking", function(req) {
+api$handle("POST", "/api/milking/", function(req) {
   new_milking_data <- jsonlite::fromJSON(req$postBody)
   mongo$insert(new_milking_data)
   return(new_milking_data)
 })
 
 # Edit existing milking data
-#* @put /milking/:id
-api$handle("PUT", "/api/milking/:id", function(req) {
+#* @put /api/milking/<id>
+api$handle("PUT","/api/milking/<id>", function(req) {
   id <- req$args$id
   updated_milking_data <- jsonlite::fromJSON(req$postBody)
   mongo$update(query = paste0('{"_id":{"$oid":"', id, '"}}'), update = paste0('{"$set":', jsonlite::toJSON(updated_milking_data), '}'))
@@ -69,8 +82,8 @@ api$handle("PUT", "/api/milking/:id", function(req) {
 })
 
 # Delete milking data
-#* @delete /milking/:id
-api$handle("DELETE", "/api/milking/:id", function(req) {
+#* @delete api/milking/:id
+api$handle("DELETE", "/api/milking/<id>", function(req) {
   id <- req$args$id
   mongo$remove(query = paste0('{"_id":{"$oid":"', id, '"}}'))
   return(list(message = "Milking data deleted successfully"))
