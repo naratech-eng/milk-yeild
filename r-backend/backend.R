995a28d3e101128ad8067b8534b00574bd024db1
snsknarayana@gmail.com <snsknarayana@gmail.com>
2024-08-21 00:38:04 -0400

    finish the backend web api for R & tested with client

--- r-backend/backend.R
+++ r-backend/backend.R
@@ -25,7 +25,7 @@ mongo_db <- Sys.getenv("MONGO_DB")
 mongo_collection <- Sys.getenv("MONGO_COLLECTION")
 
 # Connect to MongoDB
-mongo <- mongo(collection = mongo_collection, db =mongo_db, url = mongo_url)
+mongo <- mongo(collection = mongo_collection, db = mongo_db, url = mongo_url)
 
 tryCatch({
   count <- mongo$count()
@@ -34,6 +34,23 @@ tryCatch({
   cat("Failed to connect to MongoDB. Error:", e$message, "\n")
   stop("Exiting script due to MongoDB connection failure.")
 })
+#function to clean data 
+remove_single_element_arrays <- function(x) {
+  if (is.list(x)) {
+    for (name in names(x)) {
+      if (is.list(x[[name]])) {
+        if (length(x[[name]]) == 1) {
+          x[[name]] <- x[[name]][[1]]
+        } else {
+          x[[name]] <- remove_single_element_arrays(x[[name]])
+        }
+      }
+    }
+  }
+  return(x)
+}
+
+
 
 # Create a new plumber router
 api <- plumber$new()
@@ -59,26 +76,59 @@ api$handle("GET", "/", function() {
 
 # Get all milking data
 #* @get /milking
-api$handle("GET", "/api/milking/", function() {
-  milking_data <- mongo$find('{}', fields =  '{"_id": 1, "Milking Number": 1, "Duration (mm:ss)": 1, "Yield (kg)": 1, "OCC (*1000 cells/ml)": 1, "Milking Interval (hh:mm)": 1, "LF": 1, "RF": 1, "LR": 1, "RR": 1, "Udder": 1, "Milk Destination": 1}')
+api$handle("GET", "/api/milking", function(req, res) {
+  milking_data <- mongo$find(
+    '{}', 
+    fields =  '{"_id": 1, "Milking Number": 1, "Duration (mm:ss)": 1, "Yield (kg)": 1, "OCC (*1000 cells/ml)": 1, "Milking Interval (hh:mm)": 1, "LF": 1, "RF": 1, "LR": 1, "RR": 1, "Udder": 1, "Milk Destination": 1}'
+    )
+# Parse JSON string into an R object  
+  # data <- jsonlite::fromJSON(milking_data , simplifyVector = FALSE)
+
+  # Process the data to remove single-element arrays
+  # data_cleaned <- lapply(milking_data, remove_single_element_arrays)
+
+# Convert the cleaned R object back to JSON
+  # cleaned_json_string <- jsonlite::toJSON(data_cleaned, pretty = TRUE, auto_unbox = TRUE)
+
+  # Send the cleaned JSON string as the response
+  # res$setHeader("Content-Type", "application/json")
+  # res$body <- cleaned_json_string
+
   return(milking_data)
 })
 
 # Create new milking data
 #* @post /milking
-api$handle("POST", "/api/milking/", function(req) {
-  new_milking_data <- jsonlite::fromJSON(req$postBody)
-  mongo$insert(new_milking_data)
-  return(new_milking_data)
+api$handle("POST", "/api/milking", function(req) {
+  mongo$insert(req$postBody)
+  return(req$postBody)
 })
 
 # Edit existing milking data
 #* @put /api/milking/<id>
 api$handle("PUT","/api/milking/<id>", function(req) {
   id <- req$args$id
+  print(paste("from db: ", req$postBody))
+  # updated_milking_data <- jsonlite::fromJSON(req$postBody)
+  # mongo$update(query = paste0('{"_id":{"$oid":"', id, '"}}'), update = paste0('{"$set":', jsonlite::toJSON(updated_milking_data), '}'))
+  # incoming JSON to R object 
   updated_milking_data <- jsonlite::fromJSON(req$postBody)
-  mongo$update(query = paste0('{"_id":{"$oid":"', id, '"}}'), update = paste0('{"$set":', jsonlite::toJSON(updated_milking_data), '}'))
-  return(updated_milking_data)
+  print(paste("Updating data with ID:", id))
+
+    # Remove `_id` if it exists in the updated data
+  updated_milking_data$`_id` <- NULL
+
+   # Convert back to JSON string after removing `_id`
+  updated_milking_data_json <- jsonlite::toJSON(updated_milking_data, auto_unbox = TRUE)
+  
+   # Construct MongoDB query and update strings
+  query <- sprintf('{"_id": {"$oid": "%s"}}', id)
+  update <- sprintf('{"$set": %s}', updated_milking_data_json)
+  
+  # Perform update operation
+  mongo$update(query = query, update = update)
+
+  return(updated_milking_data_json)
 })
 
 # Delete milking data
@@ -90,4 +140,5 @@ api$handle("DELETE", "/api/milking/<id>", function(req) {
 })
 
 # Run the API
+
 api$run(port = 5001)
